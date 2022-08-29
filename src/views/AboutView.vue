6844cd472c9e44e89f884a2443468f4ee0982481
MazzCode <mazzcode@gmail.com>
2022-08-29 00:52:33 -0300

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
+  <div class="About">
     <h1>This is an about page</h1>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis consequuntur maiores minus! Suscipit ea excepturi iusto autem facilis aliquid consequatur veritatis non distinctio nesciunt neque ad error, quas eaque perspiciatis. Ipsam accusantium alias veniam esse delectus aut, obcaecati error sed. Consequatur quae hic officia ipsam voluptates adipisci enim nobis maxime sed vitae placeat reiciendis alias a dolores, numquam inventore atque eligendi harum sapiente pariatur nulla labore qui nesciunt! Sint, possimus reiciendis. Eum incidunt hic sed illo cumque quo quis eveniet aliquid. Officia magnam obcaecati doloremque minima voluptatum soluta dignissimos ratione sunt vero aut, itaque saepe praesentium omnis commodi assumenda perspiciatis.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis consequuntur maiores minus! Suscipit ea excepturi iusto autem facilis aliquid consequatur veritatis non distinctio nesciunt neque ad error, quas eaque perspiciatis. Ipsam accusantium alias veniam esse delectus aut, obcaecati error sed. Consequatur quae hic officia ipsam voluptates adipisci enim nobis maxime sed vitae placeat reiciendis alias a dolores, numquam inventore atque eligendi harum sapiente pariatur nulla labore qui nesciunt! Sint, possimus reiciendis. Eum incidunt hic sed illo cumque quo quis eveniet aliquid. Officia magnam obcaecati doloremque minima voluptatum soluta dignissimos ratione sunt vero aut, itaque saepe praesentium omnis commodi assumenda perspiciatis.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Home page</h1>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis consequuntur maiores minus! Suscipit ea excepturi iusto autem facilis aliquid consequatur veritatis non distinctio nesciunt neque ad error, quas eaque perspiciatis. Ipsam accusantium alias veniam esse delectus aut, obcaecati error sed. Consequatur quae hic officia ipsam voluptates adipisci enim nobis maxime sed vitae placeat reiciendis alias a dolores, numquam inventore atque eligendi harum sapiente pariatur nulla labore qui nesciunt! Sint, possimus reiciendis. Eum incidunt hic sed illo cumque quo quis eveniet aliquid. Officia magnam obcaecati doloremque minima voluptatum soluta dignissimos ratione sunt vero aut, itaque saepe praesentium omnis commodi assumenda perspiciatis.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis consequuntur maiores minus! Suscipit ea excepturi iusto autem facilis aliquid consequatur veritatis non distinctio nesciunt neque ad error, quas eaque perspiciatis. Ipsam accusantium alias veniam esse delectus aut, obcaecati error sed. Consequatur quae hic officia ipsam voluptates adipisci enim nobis maxime sed vitae placeat reiciendis alias a dolores, numquam inventore atque eligendi harum sapiente pariatur nulla labore qui nesciunt! Sint, possimus reiciendis. Eum incidunt hic sed illo cumque quo quis eveniet aliquid. Officia magnam obcaecati doloremque minima voluptatum soluta dignissimos ratione sunt vero aut, itaque saepe praesentium omnis commodi assumenda perspiciatis.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
